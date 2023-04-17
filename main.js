80d5cbc8b701904d12cf366938220bf99f4c491e
Yash Raj Vishwanath <91608542+yashrajv17@users.noreply.github.com>
2023-04-18 01:42:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -29,19 +29,20 @@ addButtonEl.addEventListener("click", function () {
 });
 
 onValue(shoppingListInDB, function (snapshot) {
-  let itemsArray = Object.entries(snapshot.val());
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val());
 
-  console.log(itemsArray);
+    clearShoppingListEl();
 
-  clearShoppingListEl();
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i];
+      let currentItemID = currentItem[0];
+      let currentItemValue = currentItem[1];
 
-  for (let i = 0; i < itemsArray.length; i++) {
-    let currentItem = itemsArray[i];
-
-    let currentItemID = currentItem[0];
-    let currentItemValue = currentItem[1];
-
-    appendItemToShoppingListEl(currentItem);
+      appendItemToShoppingListEl(currentItem);
+    }
+  } else {
+    shoppingListEl.innerHTML = "No items here... yet";
   }
 });
 
